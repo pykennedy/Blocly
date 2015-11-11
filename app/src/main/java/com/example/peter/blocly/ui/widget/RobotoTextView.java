70e9be2d8ca876ffb4b69eefb0691961f8a444a1
Peter Kennedy <pykennedy@gmail.com>
2015-11-10 21:31:52 -0800

    Checkpoint 37 in progress

--- app/src/main/java/com/example/peter/blocly/ui/widget/RobotoTextView.java
+++ app/src/main/java/com/example/peter/blocly/ui/widget/RobotoTextView.java
@@ -39,6 +39,7 @@ public class RobotoTextView extends TextView {
         TypedArray typedArray = getContext().getTheme().obtainStyledAttributes(
                 attrs, R.styleable.Roboto, 0, 0);
         int robotoFontIndex = typedArray.getInteger(R.styleable.Roboto_robotoFont, -1);
+        // try something like typedArray.getInteger(R.styleable.Robot_robotoStyle,-1);
         typedArray.recycle();
         String[] stringArray = getResources().getStringArray(R.array.roboto_font_file_names);
 
@@ -46,67 +47,24 @@ public class RobotoTextView extends TextView {
             return;
         }
 
-    /* challenge code */
-        boolean condensed = false;
-        String robotoStyle = "";
-        String parseMe = stringArray[robotoFontIndex];
-        if(parseMe.contains("Condensed"))
-            condensed = true;
-        if(parseMe.contains("Black")) robotoStyle += "Black";
-        else if(parseMe.contains("Bold")) robotoStyle += "Bold";
-        else if(parseMe.contains("Light")) robotoStyle += "Light";
-        else if(parseMe.contains("Medium")) robotoStyle += "Medium";
-        else if(parseMe.contains("Regular")) robotoStyle += "Regular";
-        else if(parseMe.contains("Thin")) robotoStyle += "Thin";
-        else robotoStyle += "";
-        if(parseMe.contains("Italic")) robotoStyle += "Italic";
-        robotoStyle += ".ttf";
-    /* challenge code */
-
-
-
         String robotoFont = stringArray[robotoFontIndex];
 
 
         Typeface robotoTypeface = null;
 
-        System.out.println(robotoFont);
-        System.err.println(robotoStyle);
+        //System.out.println(robotoFont);
 
-///* challenge code
-        if(condensed) {
-            if (sTypefaces.containsKey("RobotoCondensed-"+robotoStyle)) {
-                robotoTypeface = sTypefaces.get("RobotoCondensed-"+robotoStyle);
-            } else {
-                robotoTypeface = Typeface.createFromAsset(getResources().getAssets(),
-                        "fonts/RobotoTTF/" + "RobotoCondensed-"+robotoStyle);
-                sTypefaces.put("RobotoCondensed-"+robotoStyle, robotoTypeface);
-            }
-            setTypeface(robotoTypeface);
-        }
-        else {
-            if (sTypefaces.containsKey("Roboto-"+robotoStyle)) {
-                robotoTypeface = sTypefaces.get("Roboto-"+robotoStyle);
-            } else {
-                robotoTypeface = Typeface.createFromAsset(getResources().getAssets(),
-                        "fonts/RobotoTTF/" + "Roboto-"+robotoStyle);
-                sTypefaces.put("Roboto-"+robotoStyle, robotoTypeface);
-            }
-            setTypeface(robotoTypeface);
-        }
-//*/
-        /*
-                    if (sTypefaces.containsKey(robotoFont)) {
-                robotoTypeface = sTypefaces.get(robotoFont);
-            } else {
-                robotoTypeface = Typeface.createFromAsset(getResources().getAssets(),
-                        "fonts/RobotoTTF/" + robotoFont);
-                sTypefaces.put(robotoFont, robotoTypeface);
-            }
 
-            setTypeface(robotoTypeface);
-         */
-        System.out.println(robotoFont);
-        System.err.println(robotoStyle);
+        if (sTypefaces.containsKey(robotoFont)) {
+            robotoTypeface = sTypefaces.get(robotoFont);
+        } else {
+            robotoTypeface = Typeface.createFromAsset(getResources().getAssets(),
+                    "fonts/RobotoTTF/" + robotoFont);
+            sTypefaces.put(robotoFont, robotoTypeface);
+        }
+
+        setTypeface(robotoTypeface);
+
+        //System.out.println(robotoFont);
     }
 }
