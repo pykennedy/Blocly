c875d5672c9f1416ad3f7b1808e60ba14a0f7e00
Peter Kennedy <pykennedy@gmail.com>
2015-11-21 15:04:21 -0800

    Checkpoint 52 complete

---
+++ app/src/main/java/com/example/peter/blocly/api/model/database/table/Table.java
@@ -0,0 +1,16 @@
+package com.example.peter.blocly.api.model.database.table;
+
+import android.database.sqlite.SQLiteDatabase;
+
+public abstract class Table {
+
+    protected static final String COLUMN_ID = "id";
+
+    public abstract String getName();
+
+    public abstract String getCreateStatement();
+
+    public void onUpgrade(SQLiteDatabase writableDatabase, int oldVersion, int newVersion) {
+        // Nothing
+    }
+}
