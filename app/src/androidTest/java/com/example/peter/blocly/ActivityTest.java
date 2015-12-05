62abf1725f52ceb5a20de8203c88d54f341c2625
Peter Kennedy <pykennedy@gmail.com>
2015-12-05 14:56:46 -0800

    Assignment 60 in progress 12/5/15 1500

---
+++ app/src/androidTest/java/com/example/peter/blocly/ActivityTest.java
@@ -0,0 +1,20 @@
+package com.example.peter.blocly;
+
+import android.test.ActivityTestCase;
+
+/**
+ * Created by Peter on 12/5/2015.
+ */
+public class ActivityTest extends ActivityTestCase {
+  /*  public ActivityTest() {
+
+        super(BloclyActivity.class);
+    }
+
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+        setContext(new RenamingDelegatingContext(getContext(), "test_"));
+        createApplication();
+    } */
+}
